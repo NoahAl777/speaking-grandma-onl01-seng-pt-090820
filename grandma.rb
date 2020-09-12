d48cc315585649c3af2d9e8b4adf1197dbba097b
Noah Alston <crashlive112@gmail.com>
2020-09-12 18:55:49 +0000

    Automatically backed up by Learn

--- grandma.rb
+++ grandma.rb
@@ -2,7 +2,7 @@
 def speak_to_grandma(phrase)
   if phrase == "I LOVE YOU GRANDMA!"
     return "I LOVE YOU TOO PUMPKIN!"
-  elseif phrase == phrase.upcase
+  else phrase == phrase.upcase
     return "NO, NOT SINCE 1938!"
   else phrase == phrase.downcase
     return "HUH?! SPEAK UP, SONNY!"
